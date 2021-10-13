342b1246c3d690570777a8277b1c4fe5f4ff259e
iron72 <iron72rus@gmail.com>
2021-10-13 10:49:07 +0300

    Calculation bonus

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+        int balanceInitial = 100;
+        int bonusCost = 100;
+        int payment = 1100;
+
+        int bonus = payment / bonusCost;
+        if (payment <= 1000) {
+            bonus = 0;
+        }
+        int balance = balanceInitial + payment + bonus;
+        System.out.println("Баланс счета " + balance + " рублей");
+
+    }
 }
